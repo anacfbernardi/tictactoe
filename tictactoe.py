418459b1e69920d9976e2f706896a5ba1989860a
Lucas Mendes <lmsantos90@gmail.com>
2023-01-02 07:23:34 -0300

    testing ssh

--- tictactoe.py
+++ tictactoe.py
@@ -11,9 +11,9 @@ board = [
 
 def printBoard(board):
     print(board[0], ' | ', board[1], ' | ', board[2])
-    print('---------')
+    print('----------')
     print(board[3], ' | ', board[4], ' | ', board[5])
-    print('---------')
+    print('----------')
     print(board[6], ' | ', board[7], ' | ', board[8])
 
 
